4572023ac889e91e67a26b17f5ef184e2dc5c4ca
Bernard Aboba <bernard.aboba@gmail.com>
2017-08-22 12:40:53 -0700

    Update date

--- respec-config.js
+++ respec-config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
     "specStatus": "CG-DRAFT",
     "shortName":  "ms-ortc",
-    "publishDate": "2017-07-28",
+    "publishDate": "2017-08-22",
     "editors": [
         {   "name":       "Microsoft Corporation",
             "company":    "Microsoft",
